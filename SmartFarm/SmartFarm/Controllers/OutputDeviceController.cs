f95b8954fe442dc750efedd54a934061e1be46d4
luucongdinh113 <83592031+luucongdinh113@users.noreply.github.com>
2022-03-24 09:58:24 +0700

    Update _Layout.cshtml and add View of Input and Ouput device

---
+++ SmartFarm/SmartFarm/Controllers/OutputDeviceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartFarm.Controllers
+{
+    public class OutputDeviceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult controlDevice()
+        {
+            return View();
+        }
+    }
+}
