ef493d26bbdce024c608102ad2f9c096ecdfb44e
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-04-09 09:55:31 +0700

    Hien complete

---
+++ SmartFarm/SmartFarm/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using SmartFarm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartFarm.Services
+{
+    public interface ICustomerService
+    {
+        Task<bool> LoginAsync(string username, string password);
+        Task SignOutAsync();
+        UserInforViewModel InforUser(string UserName);
+        void PostEditAccount(UserInforViewModel account);
+        Task<bool> PostPassword(UserInforViewModel account);
+    }
+}
