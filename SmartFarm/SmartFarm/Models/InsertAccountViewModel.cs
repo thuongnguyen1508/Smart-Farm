a6bfe9f766ab625de401e4d21af9bdebe913ac9b
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-04-11 12:19:46 +0700

    Login (#10)
    * Hien complete
    
    * Login

---
+++ SmartFarm/SmartFarm/Models/InsertAccountViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartFarm.Models
+{
+    public class InsertAccountViewModel
+    {
+        [Required()]
+        public string UserName { get; set; }
+        [Required()]
+        public string Password { get; set; }
+        [Required()]
+        public string RePassword { get; set; }
+        [Required()]
+        public string Ten { get; set; }
+        [Required()]
+        public string Ho { get; set; }
+        [Required()]
+        public string Email { get; set; }
+        [Required()]
+        public string Role { get; set; }
+        [Required()]
+        public int FarmID { get; set; }
+        [Required()]
+        public string PhoneNumber { get; set; }
+        [Required()]
+        public string Address { get; set; }
+    }
+}
