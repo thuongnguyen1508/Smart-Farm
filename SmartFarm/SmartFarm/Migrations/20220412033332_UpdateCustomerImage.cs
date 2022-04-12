bab30cb5940985bcdf8f213400e3dbcc006f46c9
luucongdinh113 <83592031+luucongdinh113@users.noreply.github.com>
2022-04-12 10:40:41 +0700

    update DB

---
+++ SmartFarm/SmartFarm/Migrations/20220412033332_UpdateCustomerImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartFarm.Migrations
+{
+    public partial class UpdateCustomerImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "CUSTOMER",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "CUSTOMER");
+        }
+    }
+}
